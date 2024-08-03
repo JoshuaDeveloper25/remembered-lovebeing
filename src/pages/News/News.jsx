cca0c5e91bb4756aa1afd2bec9074265ee5b1cab
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-08-03 01:26:04 -0600

    Subido

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -3,7 +3,7 @@ import { MdPostAdd } from "react-icons/md";
 const News = () => {
   return (
     <section className="container-page">
-      {/* Introduction */}
+      {/* --> Introduction */}
       <div className="text-center mt-9 mb-14">
         <h2 className=" text-primary-color/85 text-3xl font-bold mb-1">News</h2>
       </div>

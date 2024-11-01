3727a1c044eaca3028449eb3a3144f7a8c1e3deb
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-11-01 00:32:55 -0600

    Subido

--- src/auth/PrivateRoutes.jsx
+++ src/auth/PrivateRoutes.jsx
@@ -5,7 +5,7 @@ import { useContext } from "react";
 const PrivateRoutes = () => {
   const { userInfo } = useContext(AppContext);
 
-  // --> If there's token, we can see the private stuff...
+  // --> If there's token, we can see the private stuff....
   return userInfo?.access_token ? <Outlet /> : <Navigate to={"/sign-in"} />;
 };
 

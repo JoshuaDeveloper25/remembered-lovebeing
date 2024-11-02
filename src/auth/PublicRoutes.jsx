ac41d6da3347f176c13ecb9d48a2c037e57f6be2
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-11-02 00:08:50 -0600

    Subido

--- src/auth/PublicRoutes.jsx
+++ src/auth/PublicRoutes.jsx
@@ -2,6 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import AppContext from "../context/AppProvider";
 import { useContext } from "react";
 
+// For global routes
 const PublicRoutes = () => {
   const { userInfo } = useContext(AppContext);
 

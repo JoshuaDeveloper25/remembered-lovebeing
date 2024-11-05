5c1253aa9a3882b82a5bac1d0fda3a34766f7800
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-11-05 14:47:57 -0600

    Subido

--- src/pages/MyProfiles/components/TabLinkContent.jsx
+++ src/pages/MyProfiles/components/TabLinkContent.jsx
@@ -1,3 +1,4 @@
+// This is the link of the content
 const TabLinkContent = ({ openTab, numberTab, children, idTab }) => {
   return (
     <div className={openTab === numberTab ? "block" : "hidden"} id={idTab}>

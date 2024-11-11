cc5c809172c7c5f58cd523be65dbe6600bed80c4
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-11-11 09:23:24 -0600

    Memorial status optionsgot better on click

--- src/pages/ProfileRemembered/components/FormChangeStatus.jsx
+++ src/pages/ProfileRemembered/components/FormChangeStatus.jsx
@@ -20,21 +20,23 @@ const FormChangeStatus = ({
             statusOptionSelected === "public"
               ? "bg-primary-color text-white"
               : "hover:bg-primary-color/20"
-          }  rounded-md animation-fade px-2.5 border py-2.5`}
+          }  rounded-md animation-fade border`}
         >
-          <label className="text-sm">
-            <input
-              onChange={(e) => setStatusOptionSelected(e?.target?.value)}
-              checked={statusOptionSelected === "public"}
-              value={"public"}
-              className="me-2"
-              type="radio"
-            />
+          <label className="text-sm px-2.5 py-4 block cursor-pointer">
+            <div className="flex items-center mb-1">
+              <input
+                onChange={(e) => setStatusOptionSelected(e?.target?.value)}
+                checked={statusOptionSelected === "public"}
+                value={"public"}
+                className="me-2"
+                type="radio"
+              />
 
-            <span className="font-semibold align-top">
-              All visitors can view and contribute{" "}
-              <span className="font-semibold text-green-500">*(Public)</span>
-            </span>
+              <span className="font-semibold align-top">
+                All visitors can view and contribute{" "}
+                <span className="font-semibold text-green-500">*(Public)</span>
+              </span>
+            </div>
 
             <span className="block text-xs">
               Recommended for most memorials. This option allows easy access to
@@ -48,21 +50,23 @@ const FormChangeStatus = ({
             statusOptionSelected === "private"
               ? "bg-primary-color text-white"
               : "hover:bg-primary-color/20"
-          }  rounded-md animation-fade px-2.5 border py-2 mt-3`}
+          }  rounded-md animation-fade border  mt-3`}
         >
-          <label className="text-sm">
-            <input
-              onChange={(e) => setStatusOptionSelected(e?.target?.value)}
-              checked={statusOptionSelected === "private"}
-              value={"private"}
-              className="me-2"
-              type="radio"
-            />
+          <label className="text-sm px-2.5 py-4 block cursor-pointer">
+            <div className="flex items-center mb-1">
+              <input
+                onChange={(e) => setStatusOptionSelected(e?.target?.value)}
+                checked={statusOptionSelected === "private"}
+                value={"private"}
+                className="me-2"
+                type="radio"
+              />
 
-            <span className="font-semibold align-top">
-              Visible only to me{" "}
-              <span className="font-semibold text-red-500">*(Private)</span>
-            </span>
+              <span className="font-semibold align-top">
+                Visible only to me{" "}
+                <span className="font-semibold text-red-500">*(Private)</span>
+              </span>
+            </div>
 
             <span className="block text-xs">
               Choose this option if you do not want the memorial to be visible

7e5f8f6c19a94398b71e6ec52cf0dc27020d2c6a
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-11-20 11:42:54 -0600

    back to top added

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,23 @@
 import { Outlet, ScrollRestoration } from "react-router-dom";
+import { IoIosArrowUp } from "react-icons/io";
 import Navbar from "../components/Navbar";
 import "../styles/animations.css";
 
 const Root = () => {
   return (
-    <main className="flex flex-col min-h-screen">
-      <div className="flex-1 h-full">
+    <main className="flex flex-col min-h-screen relative">
+      {/* Back to top button */}
+      <div className="fixed right-3 bottom-3 z-[9999]">
+        <a
+          className="bg-primary-color-light text-white shadow-primary-color-light rounded-full hover:opacity-55 hover:scale-105 animation-fade font-bold block px-3 py-3 shadow-md"
+          type="button"
+          href="#top"
+        >
+          <IoIosArrowUp size={24} />
+        </a>
+      </div>
+
+      <div id="top" className="flex-1 h-full">
         <Navbar />
 
         <Outlet />

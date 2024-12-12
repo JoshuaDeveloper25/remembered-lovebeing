edadac350ab6f3a67b35b2cba89de6a088a40f2c
JoshuaDeveloper25 <amandasarvelasquez@gmail.com>
2024-12-12 00:20:41 -0600

    subido

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       <CreateMemorial />
 
       {/* Recent Online Memorials */}
-      <RecentMemorialsCarousel />
+      {/* <RecentMemorialsCarousel /> */}
 
       {/* Find a Memorial */}
       <FindMemorial />
